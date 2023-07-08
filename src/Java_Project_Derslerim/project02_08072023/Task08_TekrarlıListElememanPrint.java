50383f1a22feee400b99fff0b2210e6cf1653512
igorr <iclalgorr@gmail.com>
2023-07-08 16:21:14 +0300

    tenth commit- ara commit2

---
+++ src/Java_Project_Derslerim/project02_08072023/Task08_TekrarlıListElememanPrint.java
@@ -0,0 +1,20 @@
+package Java_Project_Derslerim.project02_08072023;
+
+public class Task08_TekrarlıListElememanPrint {
+        /*
+        Girilen bir listte tekararlı elemanları print eden  metod create ediniz.
+         *
+         * n = 5 icin (listin uzunlugu);
+         * input { 1 , 2 , 2 , 3 , 4}
+         * output {2}
+         *
+         */
+        public static void main(String[] args) {
+
+    }
+
+
+
+}
+
+
